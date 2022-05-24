13860db9fddbcf059287942d95f7202cbf3cb2c0
Yaroslav <ssyarik538@gmail.com>
2022-05-24 14:46:41 +0300

    cool

--- bot2.py
+++ bot2.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord_components import DiscordComponents, Button, ButtonStyle
 import json
 import sqlite3
 from tabulate import tabulate
@@ -102,7 +101,7 @@ async def setup(ctx):
 
 
 @client.command(aliases=["claim", "create"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __claim(ctx):
 	uid = ctx.author.id
 	cursor.execute(f"SELECT author FROM counter where author={uid}")
@@ -114,7 +113,8 @@ async def __claim(ctx):
 	for row in cursor.execute(f"SELECT channel FROM counter where author={uid}"):
 		channel = row[0]
 		try:
-			channel1 = client.get_channel(channel)
+			guild = ctx.message.guild
+			channel1 = guild.get_channel(channel)
 			user= await client.fetch_user(uid)
 			await channel1.set_permissions(user, read_messages=True, send_messages=True, view_channel=True, manage_channels=True)
 		except:
@@ -125,19 +125,18 @@ async def __claim(ctx):
 	for row in cursor.execute(f"SELECT stat FROM counter where author={uid}"):
 		count = row[0]
 		if count == False:
-			for guild in client.guilds:
-				guild = client.get_guild(guild.id)
-				category = discord.utils.get(guild.categories, name="Сделки")
-				channel = await guild.create_text_channel(f'сделка-{ctx.author.name}', category=category)
-				user= await client.fetch_user(uid)
-				await channel.set_permissions(user, read_messages=True, send_messages=True, view_channel=True, manage_channels=True)
-				cursor.execute(f'UPDATE counter SET stat=True,channel = {channel.id} where author={uid}')
-				connection.commit()
-				await ctx.send(embed=discord.Embed(description=f"**Тикет <#{channel.id}> успешно создан <@{ctx.author.id}>**"))
-				await channel.send(embed=discord.Embed(description=f"**Тикет успешно создан <@{ctx.author.id}>**"))
+			guild = ctx.message.guild
+			category = discord.utils.get(guild.categories, name="Сделки")
+			channel = await guild.create_text_channel(f'сделка-{ctx.author.name}', category=category)
+			user= await client.fetch_user(uid)
+			await channel.set_permissions(user, read_messages=True, send_messages=True, view_channel=True, manage_channels=True)
+			cursor.execute(f'UPDATE counter SET stat=True,channel = {channel.id} where author={uid}')
+			connection.commit()
+			await ctx.send(embed=discord.Embed(description=f"**Тикет <#{channel.id}> успешно создан <@{ctx.author.id}>**"))
+			await channel.send(embed=discord.Embed(description=f"**Тикет успешно создан <@{ctx.author.id}>**"))
 
 @client.command(aliases=["add", "+"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __add(ctx, member: discord.Member):
 	uid = ctx.author.id
 	channel = client.get_channel(ctx.channel.id)
@@ -150,13 +149,15 @@ async def __add(ctx, member: discord.Member):
 		channel1 = client.get_channel(channel1id)
 		user= await client.fetch_user(member.id)
 		await channel1.set_permissions(user, read_messages=True, send_messages=True, view_channel=True)
-		for guild in client.guilds:
+		try:
 			role = guild.get_role(936505743987867659)
 			await member.add_roles(role)
+		except:
+			pass
 		await channel1.send(f"Здравствуйте <@{member.id}>.",embed=discord.Embed(description=f"**Участник <@{member.id}> успешно добавлен в <#{channel1.id}>.**"))
 
 @client.command(aliases=["del", "-"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __del(ctx, member: discord.Member):
 	uid = ctx.author.id
 	channel = client.get_channel(ctx.channel.id)
@@ -169,48 +170,51 @@ async def __del(ctx, member: discord.Member):
 		channel1 = client.get_channel(channel1id)
 		user= await client.fetch_user(member.id)
 		await channel1.set_permissions(user, read_messages=False, send_messages=False, view_channel=False)
-		for guild in client.guilds:
+		try:
 			role = guild.get_role(936505743987867659)
 			await member.remove_roles(role)
+		except:
+			pass
 		await channel1.send(embed=discord.Embed(description=f"**Участник <@{member.id}> успешно удалён из <#{channel1.id}>.**"))
 
 @client.command(aliases=["close"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __close(ctx):
 	uid = ctx.author.id
-	channel = client.get_channel(ctx.channel.id)
+	guild = ctx.message.guild
+	channel = guild.get_channel(ctx.channel.id)
 	cursor.execute(f"SELECT author FROM counter where author={uid}")
 	if cursor.fetchone() == None:
 		cursor.execute(f"INSERT INTO counter VALUES (False, {uid}, 0)")
 		connection.commit()
 	for row in cursor.execute(f"SELECT channel FROM counter where author={uid}"):
 		channel1id = row[0]
-		channel1 = client.get_channel(channel1id)
+		channel1 = guild.get_channel(channel1id)
 		await channel1.delete()
 		
 
 @client.command(aliases=["vip", "вип"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __vip(ctx):
 	await ctx.reply(embed=discord.Embed(description=f"**Приватный сервер в адопте: [Нажми сюда что-бы зайти](https://www.roblox.com/games/920587237?privateServerLinkCode=41122651371977856802806669923465)**"))
 
 @client.command(aliases=["vipmm", "випмм2", "vipmm2","випмм"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __vipmm2(ctx):
 	await ctx.reply(embed=discord.Embed(description=f"**Приватный сервер в мардер мистери: [Нажми сюда что-бы зайти](https://www.roblox.com/games/142823291?privateServerLinkCode=89852226291968909722581151698927)**"))
 
 @client.command(aliases=["vippsx", "виппсх", "виппетсим","vipps","виппет"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __vippsx(ctx):
 	await ctx.reply(embed=discord.Embed(description=f"**Приватный сервер в пет симулятор X: [Нажми сюда что-бы зайти](https://www.roblox.com/games/6284583030/x3-Pet-Simulator-X?privateServerLinkCode=55975160176260713274764851250283)**"))
 
 @client.command(aliases=["check"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __check(ctx):
 	await ctx.reply(embed=discord.Embed(description=f"**Проверить свой пендинг: [Нажми сюда что-бы проверить](https://www.roblox.com/transactions)**"))
 
 @client.command(aliases=["tutorial", "туториал"])
-@commands.has_role(933672340150165714 or 926469565343477760 or 953275659218743337)
+@commands.has_any_role('👨‍👧‍👦 | Посредник')
 async def __tutorial(ctx):
 	embed = discord.Embed(description=f"**Туториал как настроить вип:**")
 	embed.set_image(url = "https://rblx.ru/images/roblox_buy.gif")
---
+++ debuger.bat
@@ -0,0 +1,4 @@
+@echo off
+
+python bot2.py
+pause
