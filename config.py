53afc10819e988dca96d8bc06ff8d4119c2d312e
Borusik1 <106591966+Borusik1@users.noreply.github.com>
2022-07-17 16:21:01 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-	"DB_URI"  : 'postgres://etdqcmbnqseqpe:21406b78e6a94b332e1d790b5246565f1144098fd1a95e4fd4d1d8fd4e0ccc07@ec2-54-194-211-183.eu-west-1.compute.amazonaws.com:5432/d3bkqd4pplu1ba',
-	"APP_URL" : 'https://discordbot188548.herokuapp.com/',
-	"PREFIX" : "?",
-	"TOKEN" : 'OTE3MDMwMjM4NTk1MjA3MTY4.Yayw9g.1rYs1KWblY90oTXxwGXPlsTXdOQ'
-}
